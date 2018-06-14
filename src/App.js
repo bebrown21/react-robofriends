6d6c316b3fc0bfbf1367625b4ab321fa9e1ac72d
Brandon Brown <Brandon.Brown@metova.com>
2018-06-14 13:39:20 -0500

    fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
-
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response =>  response.json())
+      .then(users => this.setState({ robots: users }));
   }
   
   onSearchChange = (event) => {
@@ -24,7 +28,7 @@ class App extends Component {
     
     return (
       <div className='tc'>
-        <h1>Robofriends</h1>
+        <h1 className='f1'>Robofriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
         <CardList robots={filteredRobots}/>
       </div>
